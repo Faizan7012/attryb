06d9c086d78d87734f7a908acc25e05c533d12ac
Faizan7012 <frtoons7012@gmail.com>
2023-05-29 11:19:48 +0530

    new

--- backend/config/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-require('dotenv').config();
-const connection = async () => {
-  return mongoose.connect(process.env.DB_URL);
-};
-
-module.exports = connection;
